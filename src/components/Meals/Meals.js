cae1261d6d8a0bfbcff90002141be5a78d2d18e2
anikahammedkhan <anikkhan1105@gmail.com>
2022-09-28 20:59:19 +0600

    design-completed

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import './Meals.css';
 
 const Meals = () => {
-    // fetch meals from mealdb api
     const [meals, setMeals] = React.useState([]);
     React.useEffect(() => {
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
@@ -11,21 +11,25 @@ const Meals = () => {
     return (
         <div>
             <h1 className='text-center text-xl mt-7 font-bold'>Available Meals</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5'>
-                {
-                    meals.map(meal => <div className='shadow-lg rounded-lg overflow-hidden'>
-                        <img src={meal.strMealThumb} alt="" />
-                        <div className='p-4'>
-                            <h1 className='text-center text-xl font-bold'>{meal.strMeal}</h1>
-                            <p className='text-center'>{meal.strInstructions.slice(0, 100)}</p>
-                        </div>
-                        {/* add two button */}
-                        <div className='flex justify-evenly items-center p-4'>
-                            <button className='bg-green-500 text-white px-4 py-2 rounded-lg'>Details</button>
-                            <button className='bg-red-500 text-white px-4 py-2 rounded-lg'>Order Now</button>
-                        </div>
-                    </div>)
-                }
+            <div className='meals-container'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
+                    {
+                        meals.map(meal => <div className='shadow-lg rounded-lg overflow-hidden'>
+                            <img src={meal.strMealThumb} alt="" />
+                            <div className='p-4'>
+                                <h1 className='text-center text-xl font-bold'>{meal.strMeal}</h1>
+                                <p className='text-center'>{meal.strInstructions.slice(0, 100)}</p>
+                            </div>
+                            <div className='flex justify-evenly items-center p-4'>
+                                <button className='bg-green-500 text-white px-4 py-2 rounded-lg'>Details</button>
+                                <button className='bg-red-500 text-white px-4 py-2 rounded-lg'>Order Now</button>
+                            </div>
+                        </div>)
+                    }
+                </div>
+                <div className='cart-container'>
+                    <h1 className='font-bold text-lg my-4'>Your Cart</h1>
+                </div>
             </div>
 
         </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            <div className="navbar bg-base-100 drop-shadow-lg rounded-lg mb-4 sticky">
+        <div className='navbar-container'>
+            <div className="navbar bg-base-100 drop-shadow-lg rounded-lg mb-4">
                 <div className="flex-1">
                     <a className="btn btn-ghost normal-case text-xl" href='/'>Meal Store</a>
                 </div>
-                <div className="flex-none">
+                <div className="flex-none hidden md:block">
                     <ul className="menu menu-horizontal p-0">
                         <li><a href='/home'>Home</a></li>
                         <li><a href='/shop'>Shop</a></li>
