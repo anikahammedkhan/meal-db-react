149a8ae25616928e6321c36f3bb6716ef5f62ecc
anikahammedkhan <anikkhan1105@gmail.com>
2022-09-28 14:47:48 +0600

    navbar and search field added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 drop-shadow-lg rounded-lg mb-4 sticky">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl" href='/'>Meal Store</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal p-0">
+                        <li><a href='/home'>Home</a></li>
+                        <li><a href='/shop'>Shop</a></li>
+                        <li><a href='/contact'>Contact Us</a></li>
+                        <li><a href='/about'>About Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
